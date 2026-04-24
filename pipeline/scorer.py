626de442075f2f6718bb70fc08b0322fc5f146a5
Karthik Kolli <karthikkolli17@gmail.com>
2026-04-24 01:15:41 -0500

    Added scorer.py with VADER and TextBlob sentiment scoring

---
+++ pipeline/scorer.py
@@ -0,0 +1,21 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from textblob import TextBlob
+
+analyzer = SentimentIntensityAnalyzer()
+
+def score_article(text):
+
+    text = str(text)
+
+    vader_scores = analyzer.polarity_scores(text)
+
+    blob = TextBlob(text)
+
+    return {
+        "vader_compound" : vader_scores["compound"],
+        "vader_positive" : vader_scores["pos"],
+        "vader_neutral" : vader_scores["neu"],
+        "vader_negative" : vader_scores["neg"],
+        "textblob_polarity" : blob.sentiment.polarity,
+        "textblob_subjectivity" : blob.sentiment.subjectivity,        
+    }
